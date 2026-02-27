9a1eb42fa48f189e1cb261824a2a182f014320e3
Swaraj Ladke <swarajladke20@gmail.com>
2026-02-27 09:28:01 +0530

    Configure for deployment: added requirements.txt, Procfile, and updated port binding

--- app.py
+++ app.py
@@ -749,5 +749,7 @@ def train_model():
 # -------------------- RUN --------------------
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
 
